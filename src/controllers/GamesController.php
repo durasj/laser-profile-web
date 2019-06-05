dbfe84cf830e91dc00f3516759eeeddf3bc27c77
Jakub Ďuraš <jakub@duras.me>
2019-06-05 14:23:56 +0200

    Revert players

--- src/controllers/GamesController.php
+++ src/controllers/GamesController.php
@@ -15,33 +15,6 @@ class GamesController extends CRUDController
         $items = $this->modelClass::all()->map(function ($game) {
             $points = rand(-1200, 4300);
             $game->points = $points;
-            $game->players = [
-                [
-                    "name" => "R4Tman",
-                    "k/d" => "34/20",
-                    "points" => 4275
-                ],
-                [
-                    "name" => "lorem",
-                    "k/d" => "25/20",
-                    "points" => $points
-                ],
-                [
-                    "name" => "XxX",
-                    "k/d" => "25/25",
-                    "points" => 1250
-                ],
-                [
-                    "name" => "Trinity",
-                    "k/d" => "25/25",
-                    "points" => -210
-                ],
-                [
-                    "name" => "CampingFTW",
-                    "k/d" => "5/14",
-                    "points" => -1210
-                ]
-            ];
             return $game;
         });
 
