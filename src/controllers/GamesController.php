962ac21db0c609fcf4764267029fbea64a6cb58b
Jakub Ďuraš <jakub@duras.me>
2019-06-05 14:11:09 +0200

    Add fake points to games

--- src/controllers/GamesController.php
+++ src/controllers/GamesController.php
@@ -3,8 +3,20 @@
 namespace Controllers;
 
 use Models\Game;
+use Slim\Http\Request;
+use Slim\Http\Response;
 
 class GamesController extends CRUDController
 {
     protected $modelClass = Game::class;
+
+    protected function getAll(Request $request, Response $response)
+    {
+        $items = $this->modelClass::all()->map(function ($game) {
+            $game->points = rand(-1200, 4300);
+            return $game;
+        });
+
+        return $response->withJson($items);
+    }
 }
